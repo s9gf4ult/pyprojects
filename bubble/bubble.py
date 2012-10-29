451c78c9d25f514cb7d4a9ce6f4b9188d61c2ca0
evgen <evgen@funtoo>
2012-10-29 09:58:18 +0600

    DONE!!! modified:   bubble.py

--- bubble/bubble.py
+++ bubble/bubble.py
@@ -6,9 +6,11 @@ print("изначальне моссиве")
 print (a,' \n')
 print("достаем лучеметэ")
 
-for i in range(len(a)-1):   # цыкл по выталкиванию наибольшего элемента 
-    if a[i] > a[i+1]:
-        t = a[i]
-        a[i] = a[i+1]
-        a[i+1] = t
+for i in range(len(a), 0, -1):
+
+    for i in range(len(a)-1):   # цыкл по выталкиванию наибольшего элемента 
+        if a[i] > a[i+1]:
+            t = a[i]
+            a[i] = a[i+1]
+            a[i+1] = t
     print (a, '\n')
