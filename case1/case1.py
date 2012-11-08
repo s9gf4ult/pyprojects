ccc97f73d79b3b446cffd8182d5d2db2a071aec9
evgen <evgen@funtoo>
2012-11-08 16:40:05 +0600

    modified:   case1.py

--- case1/case1.py
+++ case1/case1.py
@@ -6,15 +6,16 @@ b = sorted(a)
 print(b)
 res = []
 k = 0
+z = 0 "решения не кошерное
 
 for i in range(len(b)-1):
     if b[i+1]-b[i] > 5:
         List = b[k:i+1]
-        if k != 0:
-            List.pop(0)
-        else:
+        if z == 0:
             pass
+        else:
+            List.pop(0)
         res.append(List)
         k = i
+        z = 1
         print(res)
-#    k = i
