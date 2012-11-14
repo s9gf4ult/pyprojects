79b5a85efaa350565bc4879d9628f6473e133323
evgen <evgen@funtoo>
2012-11-14 12:49:32 +0600

    remove z	modified:   case1.py

--- case1/case1.py
+++ case1/case1.py
@@ -7,16 +7,10 @@ r = range(len(b))
 print(b)
 res = []
 k = 0
-z = 0
 
 for i,j in zip(r,r[1:]):
     if b[j]-b[i] > 5:
         spisok = b[k:i+1]
-        if z == 0:
-            pass
-        else:
-            spisok.pop(0)
-        res.append(List)
-        k = i
-        z = 1
+        res.append(spisok)
+        k = i+1
         print(res)
