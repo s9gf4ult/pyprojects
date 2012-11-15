dfd65d4d8c14347605da4f821f7f3eb4ee2a62a4
axce1 <axce1.funtoo@gmail.com>
2012-11-15 21:07:39 +0500

    modified:   alg_e.py

--- algmE/alg_e.py
+++ algmE/alg_e.py
@@ -3,13 +3,20 @@ m = 119
 n = 544
 
 
+
 t = m
 m = n
 n = t
 
-r = m % n
 
-if r == 0:
-    print ('alarm')
-else:
-    print (r)
+while True:
+    r = m % n
+
+    if r == 0:
+        print ('alarm')
+        print (n)
+        break
+    else:
+        m = n
+        n = r
+
