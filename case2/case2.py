18273bc74be80ad5bbad3cd72370f6bc5777b7fc
evgen <evgen@funtoo>
2012-11-16 17:28:00 +0600

    new file:   case2.py

---
+++ case2/case2.py
@@ -0,0 +1,21 @@
+a = int(input("Ебашим лук: "))
+b = int(input("Лук не торт! Ебашь еще: "))
+
+aspl = []
+bspl = []
+
+x = 1
+
+if a > b:
+    t = a
+    a = b
+    b = t
+
+while x <= a :
+    if a % x == 0:
+        aspl.append(x)
+    if b % x == 0:
+        bspl.append(x)
+x = x+1
+
+print (aspl, bspl)
