8e3c363ad45504a20fa0e88f06ece05526651e46
axce1 <axce1.funtoo@gmail.com>
2012-11-18 18:08:41 +0500

    def case3 done

--- case3/def_case3.py
+++ case3/def_case3.py
@@ -1,24 +1,16 @@
 import random
 
-def solve(a):
+def solve(sp):
 
-    sp = []
     zero = []
     nonzero = []
-
-    for i in range(a):
-        p = random.randint(-100, 100)
-        sp.append(p)
-
-    print (sp)
 
     for j in range(len(sp)):
         if sp[j] < 0:
             zero.append(sp[j])
         else:
             nonzero.append(sp[j])
-    print (zero)
-    print (nonzero)
+    return (zero, nonzero)
 
 if __name__ == '__main__':
     solve(100)
