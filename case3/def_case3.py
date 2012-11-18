acd6fc1378799448e31cb758a88d71d5d6e814a9
axce1 <axce1.funtoo@gmail.com>
2012-11-18 10:36:26 +0500

    new file:   case3.py 	new file:   def_case3.py

---
+++ case3/def_case3.py
@@ -0,0 +1,21 @@
+import random
+
+def solve():
+    sp = []
+    zero = []
+    nonzero = []
+
+    for i in range(10):
+        p = random.randint(-100, 100)
+        sp.append(p)
+
+    print (sp)
+
+    for j in range(len(sp)):
+        if sp[j] < 0:
+            zero.append(sp[j])
+        else:
+            nonzero.append(sp[j])
+    print (zero)
+    print (nonzero)
+
