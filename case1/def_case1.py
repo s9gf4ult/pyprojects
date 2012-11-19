189344de6063b341d5a9ad74e393c5a9d288af36
evgen <evgen@funtoo>
2012-11-19 14:32:22 +0600

    modified:   def_case1.py

--- case1/def_case1.py
+++ case1/def_case1.py
@@ -2,24 +2,19 @@ import random
 
 def spisok():
 
-    a = sorted(random.sample(range(100), 10))   # генерируем и сортируем список
+    a = sorted(random.sample(range(100), 10))
     print (a, '\n')
 
-    # объявляем переменные
     res = []
     k = 0
     z = 0
 
     for i in range(len(a)-1):
         if a[i+1] - a[i] >5:
-            List = a[k:i+1]
-            if z == 0:
-                pass
-            else:
-                List.pop(0)
-            res.append(List)
-            k = i
-            z += 0
+            sp = a[k:i+1]
+            res.append(sp)
+            k = i+1
+    res.append(a[k:])
     print(res)
 
 
