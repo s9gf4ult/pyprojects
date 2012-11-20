a2cca0b97d231abb33d6a706ccc749e8050a9f4c
evgen <evgen@funtoo>
2012-11-20 09:37:13 +0600

    modified:   def_case1.py

--- case1/def_case1.py
+++ case1/def_case1.py
@@ -1,11 +1,12 @@
 def spisok(q):
 
     a = sorted(q)
+    r = range(len(a))
     res = []
     k = 0
 
-    for i in range(len(a)-1):
-        if a[i+1] - a[i] >5:
+    for i,j in zip(r,r[1:]):
+        if a[j] - a[i] >50:
             sp = a[k:i+1]
             res.append(sp)
             k = i+1
