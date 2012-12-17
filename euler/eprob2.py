8008fa9c74b493fb7ed20ab7adbd744e4ca9c911
evgen <evgen@funtoo>
2012-12-17 17:39:18 +0600

    new problem

---
+++ euler/eprob2.py
@@ -0,0 +1,10 @@
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million,
+# find the sum of the even-valued terms. even - chetnie chisla
+#
+
+count = 0
+fib = []
+for i in range(10):
+    num = fib[i]+fib[i+1]
+    print num
+
