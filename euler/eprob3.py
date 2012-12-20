573a6725dde628e870fba4af914800bbdd56418d
axce1 <axce1.funtoo@gmail.com>
2012-12-20 22:49:32 +0500

    proble 3 done

--- euler/eprob3.py
+++ euler/eprob3.py
@@ -1,7 +1,11 @@
+#
+# need optimize
+#
+
 p = 2
-n = list(range(120))
-#n = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+n = list(range(7000))
 z = len(n)
+
 while z != p:
     for i in range(len(n)):
         q = p * i
@@ -9,5 +13,10 @@ while z != p:
             n.remove(q)
     p += 1
 
-print n
-
+dig = 600851475143
+for q in n:
+    if dig % q == 0 and q != 1:
+        #print dig / q
+        print q
+        dig = dig/q
+       # break
