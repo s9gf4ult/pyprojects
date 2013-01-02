a7054e477d257e15e83fa713ea1c990ea826c4f7
evgen <evgeniypopov@gmail.com>
2013-01-02 21:18:14 +0600

    dont close socket after 1 message

--- client_server/client.py
+++ client_server/client.py
@@ -2,10 +2,18 @@
 import socket
 
 serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host = socket.gethostname()
+host = 'localhost'
 port = 30033
 
 serversocket.connect((host,port))
 
-print serversocket.recv(1024)
-serversocket.close
+while True:
+    buf = raw_input(">> ")
+    serversocket.send(buf)
+    result = serversocket.recv(1024)
+    print result
+
+    if buf == "exit":
+        break
+
+serversocket.close()
